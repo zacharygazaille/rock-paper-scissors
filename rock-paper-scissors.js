73a4135f1723a8528578b778b2658fccc0829ca7
Zachary Gazaille <zacharygazaille@gmail.com>
2024-07-06 22:01:54 -0400

    Add a function to get computer choice
    getComputerChoice function chooses a random number from 0 to 2 and
    returns a string with "rock", "paper" or "scissors".

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
