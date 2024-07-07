b8532d39c077c66a24dd7a9f030c4ec09d733961
Zachary Gazaille <zacharygazaille@gmail.com>
2024-07-06 22:18:50 -0400

    Add a function to get human choice
    getHumanChoice function prompts user for a choice between rock, paper
    and scissors and returns it.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,3 +9,21 @@ function getComputerChoice() {
             return "scissors";
     }
 }
+
+function getHumanChoice() {
+    while (true) {
+        let choice = prompt("1. rock\n2. paper\n3. scissors\nPlease pick: ");
+        if (choice == 1) {
+            return ("rock");
+            break;
+        } else if (choice == 2) {
+            return ("paper");
+            break;
+        } else if (choice == 3) {
+            return ("scissors");
+            break;
+        } else {
+            console.log("Please enter a number 1-3.");
+        }
+    }
+}
