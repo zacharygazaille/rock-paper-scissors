d7d83420aed6b6ca70aaad1d951f5a43ccd3d0c6
Zachary Gazaille <zacharygazaille@gmail.com>
2024-07-07 17:39:14 -0400

    Add logic for a round of the game
    Adds logic by choosing a winner at the end of the round and incrementing
    the score of the winner of the round.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -23,11 +23,34 @@ function getHumanChoice() {
     }
 }
 
-console.log(getHumanChoice());
-
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-
+    if (humanSelection == computerSelection) {
+        console.log("Round ends in a tie!");
+    } else if (humanSelection == "rock" && computerSelection == "paper") {
+        console.log("You lose! Paper beats Rock.")
+        computerScore++;
+    } else if (humanSelection == "rock" && computerSelection == "scissors") {
+        console.log("You win! Rock beats Scissors.")
+        humanScore++;
+    } else if (humanSelection == "paper" && computerSelection == "rock") {
+        console.log("You win! Paper beats Rock.")
+        humanScore++;
+    } else if (humanSelection == "paper" && computerSelection == "scissors") {
+        console.log("You lose! Scissors beats Paper.")
+        computerScore++;
+    } else if (humanSelection == "scissors" && computerSelection == "rock") {
+        console.log("You lose! Rock beats Scissors.")
+        computerScore++;
+    } else if (humanSelection == "scissors" && computerSelection == "paper") {
+        console.log("You win! Scissors beats Paper.")
+        humanScore++;
+    }
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
